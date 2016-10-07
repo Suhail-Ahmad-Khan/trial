e2d97487e8f3a6fb6c7b1cd7ad2cafda4f0db658
khansuhl <khansuhl@gmail.com>
2016-10-07 11:33:35 +0530

    edited

--- prog1.c
+++ prog1.c
@@ -4,4 +4,11 @@ int main()
 int a=10, b=20, c;
 c=a+b;
 printf("output: %d\n", c);
+int d, e, f;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("output of sub: %d\n", d);
+printf("output of mul: %d\n", e);
+printf("output of div: %d\n", f);
 }
